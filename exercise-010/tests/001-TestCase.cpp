ce1fbcc6d0d19154428ca0ba017691beaba38924
FinjaVetter <vetter.finja-it24@it.dhbw-ravensburg.de>
2025-11-11 14:43:56 +0100

    added new test cases

--- exercise-010/tests/001-TestCase.cpp
+++ exercise-010/tests/001-TestCase.cpp
@@ -7,5 +7,37 @@ TEST_CASE("MyVector basic functionality") {
     SECTION("initial size is zero") {
         REQUIRE(v.size() == 0);
     }
-    // ... weitere Tests ...
+
+    SECTION("push_back increases size and stores values") {
+        v.push_back(10);
+        v.push_back(20);
+        v.push_back(30);
+        REQUIRE(v.size() == 3);
+        REQUIRE(v.at(0) == 10);
+        REQUIRE(v[1] == 20);
+        REQUIRE(v.at(2) == 30);
+    }
+
+    SECTION("resize increases size and default initializes new elements") {
+        v.push_back(5);
+        v.resize(5);
+        REQUIRE(v.size() == 5);
+        REQUIRE(v.at(0) == 5);
+        for (unsigned int i = 1; i < v.size(); ++i) {
+            REQUIRE(v.at(i) == 0);
+        }
+    }
+
+    SECTION("clear resets the vector size") {
+        v.push_back(42);
+        v.clear();
+        REQUIRE(v.size() == 0);
+    }
+
+    SECTION("at throws out_of_range for invalid indexes") {
+        REQUIRE_THROWS_AS(v.at(0), std::out_of_range);
+        v.push_back(1);
+        REQUIRE_NOTHROW(v.at(0));
+        REQUIRE_THROWS_AS(v.at(100), std::out_of_range);
+    }
 }
